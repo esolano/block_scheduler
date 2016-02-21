a082a489212209b2acacbd2f18c489d49e096ee5
Nishanth Krishnan <nishanth.flip@gmail.com>
2016-02-21 17:23:14 +0530

    Initial commit.

---
+++ src/Plugin/Condition/Expiry.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\block_expire\Plugin\Condition;
+
+use Drupal\Core\Condition\ConditionPluginBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+
+
+
+/**
+ * Provides a 'Expiry ' condition.
+ *
+ * @Condition(
+ *   id = "expiry",
+ *   label = @Translation("Expiry")
+ * )
+ *
+ */
+class Expiry extends ConditionPluginBase   {
+
+/**
+   * {@inheritdoc}
+   */
+ 
+public function summary() {
+
+	return t('Expiry');
+
+}
+
+public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+   $default_start = (!empty($this->configuration['start']) )? DrupalDateTime::createFromTimestamp($this->configuration['start']): '';  
+   $default_end = (!empty($this->configuration['end'])) ? DrupalDateTime::createFromTimestamp($this->configuration['end']) : '';
+   $form['start'] = array(
+  '#type' => 'datetime',
+  '#title' => t('Publish Date'),
+  '#default_value'  =>$default_start );
+
+   $form['end'] = array(
+  '#type' => 'datetime',
+  '#title' => t('Expiry Date'),
+  '#default_value' => $default_end );
+
+  return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+     
+     if (is_object($form_state->getValue('start'))) { 
+     	 $this->configuration['start'] = $form_state->getValue('start')->getTimestamp();
+    
+     }
+     if (is_object( $form_state->getValue('end') )) {
+     	$this->configuration['end'] = $form_state->getValue('end')->getTimestamp();
+     }
+ 
+    parent::submitConfigurationForm($form, $form_state);
+  }
+
+ /**
+   * {@inheritdoc}
+   */
+  public function evaluate() {
+  $status= true;
+
+   if (empty( $this->configuration['start']) &&  empty( $this->configuration['end']) && !$this->isNegated()) {
+      return TRUE;
+    }
+    if (!empty( $this->configuration['start'])) {
+      $status= $status && time()>=$this->configuration['start'];
+    }
+
+    if (!empty( $this->configuration['end'])) {
+      $status= $status && time()<=$this->configuration['end'];
+    }
+
+
+return $status;
+
+
+  }
+
+  public function defaultConfiguration() {
+    return array('start' => '','end'=>'') + parent::defaultConfiguration();
+  }
+
+}
