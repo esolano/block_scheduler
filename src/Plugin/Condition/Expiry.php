6d896eed17f0f20b5176c073cbb56e264477519f
Nishanth Krishnan <nishkris@329224.no-reply.drupal.org>
2016-02-22 14:50:44 +0530

    php code sniffer fixes

--- src/Plugin/Condition/Expiry.php
+++ src/Plugin/Condition/Expiry.php
@@ -22,70 +22,70 @@ class Expiry extends ConditionPluginBase
     /**
  * {@inheritdoc}
  */
-public function summary()
-{
-    return t('Expiry');
-}
-/**
+    public function summary()
+    {
+        return t('Expiry');
+    }
+    /**
  * {@inheritdoc}
  */
-public function buildConfigurationForm(array $form, FormStateInterface $form_state)
-{
-    $default_start = (!empty($this->configuration['start'])) ? DrupalDateTime::createFromTimestamp($this->configuration['start']) : '';
-    $default_end = (!empty($this->configuration['end'])) ? DrupalDateTime::createFromTimestamp($this->configuration['end']) : '';
-    $form['start'] = array(
-  '#type' => 'datetime',
-  '#title' => t('Publish Date'),
-  '#default_value' => $default_start, );
+    public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+    {
+        $default_start = (!empty($this->configuration['start'])) ? DrupalDateTime::createFromTimestamp($this->configuration['start']) : '';
+        $default_end = (!empty($this->configuration['end'])) ? DrupalDateTime::createFromTimestamp($this->configuration['end']) : '';
+        $form['start'] = array(
+        '#type' => 'datetime',
+        '#title' => t('Publish Date'),
+        '#default_value' => $default_start, );
 
-    $form['end'] = array(
-  '#type' => 'datetime',
-  '#title' => t('Expiry Date'),
-  '#default_value' => $default_end, );
+        $form['end'] = array(
+        '#type' => 'datetime',
+        '#title' => t('Expiry Date'),
+        '#default_value' => $default_end, );
 
-    return parent::buildConfigurationForm($form, $form_state);
-}
+        return parent::buildConfigurationForm($form, $form_state);
+    }
 
-  /**
+    /**
    * {@inheritdoc}
    */
-  public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
-  {
-      if (is_object($form_state->getValue('start'))) {
-          $this->configuration['start'] = $form_state->getValue('start')->getTimestamp();
-      }
-      if (is_object($form_state->getValue('end'))) {
-          $this->configuration['end'] = $form_state->getValue('end')->getTimestamp();
-      }
+    public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
+    {
+        if (is_object($form_state->getValue('start'))) {
+            $this->configuration['start'] = $form_state->getValue('start')->getTimestamp();
+        }
+        if (is_object($form_state->getValue('end'))) {
+            $this->configuration['end'] = $form_state->getValue('end')->getTimestamp();
+        }
 
-      parent::submitConfigurationForm($form, $form_state);
-  }
+        parent::submitConfigurationForm($form, $form_state);
+    }
 
-  /**
+    /**
    * {@inheritdoc}
    */
-  public function evaluate()
-  {
-      $status = true;
-      if (empty($this->configuration['start']) &&  empty($this->configuration['end']) && !$this->isNegated()) {
-          return true;
-      }
-      if (!empty($this->configuration['start'])) {
-          $status = $status && time() >= $this->configuration['start'];
-      }
+    public function evaluate()
+    {
+        $status = true;
+        if (empty($this->configuration['start']) &&  empty($this->configuration['end']) && !$this->isNegated()) {
+            return true;
+        }
+        if (!empty($this->configuration['start'])) {
+            $status = $status && time() >= $this->configuration['start'];
+        }
 
-      if (!empty($this->configuration['end'])) {
-          $status = $status && time() <= $this->configuration['end'];
-      }
+        if (!empty($this->configuration['end'])) {
+            $status = $status && time() <= $this->configuration['end'];
+        }
 
-      return $status;
-  }
+        return $status;
+    }
 
-  /**
+    /**
    * {@inheritdoc}
    */
-  public function defaultConfiguration()
-  {
-      return array('start' => '', 'end' => '') + parent::defaultConfiguration();
-  }
+    public function defaultConfiguration()
+    {
+        return array('start' => '', 'end' => '') + parent::defaultConfiguration();
+    }
 }
