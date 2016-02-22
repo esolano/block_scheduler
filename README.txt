9f160b9bbef95f437e2d2cc3da272e9f014b1dbf
Nishanth Krishnan <nishkris@329224.no-reply.drupal.org>
2016-02-22 18:44:37 +0530

    Coding standards checks

--- README.txt
+++ README.txt
@@ -1,5 +1,10 @@
 INTRODUCTION
 ------------
+The Block expire module is condition plugin which adds configuartion
+form in block with publish and expiry dates
 
-The Block expire module is condition plugin which adds configuartion form in block with publish and expiry dates
+ * For a full description of the module, visit the project page:
+   https://www.drupal.org/sandbox/nishkris/2672728
 
+ * To submit bug reports and feature suggestions, or to track changes:
+   https://www.drupal.org/project/issues/2672728
--- src/Plugin/Condition/Expiry.php
+++ src/Plugin/Condition/Expiry.php
@@ -3,6 +3,7 @@
  * @file
  * Contains \Drupal\block_expire\\Plugin\Condition\Expiry.
  */
+
 namespace Drupal\block_expire\Plugin\Condition;
 
 use Drupal\Core\Condition\ConditionPluginBase;
@@ -17,75 +18,77 @@ use Drupal\Core\Datetime\DrupalDateTime;
  *   label = @Translation("Expiry")
  * )
  */
-class Expiry extends ConditionPluginBase
-{
-    /**
- * {@inheritdoc}
- */
-    public function summary()
-    {
-        return t('Expiry');
-    }
-    /**
- * {@inheritdoc}
- */
-    public function buildConfigurationForm(array $form, FormStateInterface $form_state)
-    {
-        $default_start = (!empty($this->configuration['start'])) ? DrupalDateTime::createFromTimestamp($this->configuration['start']) : '';
-        $default_end = (!empty($this->configuration['end'])) ? DrupalDateTime::createFromTimestamp($this->configuration['end']) : '';
-        $form['start'] = array(
-        '#type' => 'datetime',
-        '#title' => t('Publish Date'),
-        '#default_value' => $default_start, );
-
-        $form['end'] = array(
-        '#type' => 'datetime',
-        '#title' => t('Expiry Date'),
-        '#default_value' => $default_end, );
-
-        return parent::buildConfigurationForm($form, $form_state);
-    }
-
-    /**
+class Expiry extends ConditionPluginBase {
+  /**
    * {@inheritdoc}
    */
-    public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
-    {
-        if (is_object($form_state->getValue('start'))) {
-            $this->configuration['start'] = $form_state->getValue('start')->getTimestamp();
-        }
-        if (is_object($form_state->getValue('end'))) {
-            $this->configuration['end'] = $form_state->getValue('end')->getTimestamp();
-        }
+  public function summary() {
 
-        parent::submitConfigurationForm($form, $form_state);
-    }
-
-    /**
+    return t('Expiry');
+  }
+  /**
    * {@inheritdoc}
    */
-    public function evaluate()
-    {
-        $status = true;
-        if (empty($this->configuration['start']) &&  empty($this->configuration['end']) && !$this->isNegated()) {
-            return true;
-        }
-        if (!empty($this->configuration['start'])) {
-            $status = $status && time() >= $this->configuration['start'];
-        }
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
 
-        if (!empty($this->configuration['end'])) {
-            $status = $status && time() <= $this->configuration['end'];
-        }
+    $default_start = (!empty($this->configuration['start'])) ? DrupalDateTime::createFromTimestamp($this->configuration['start']) : '';
+    $default_end = (!empty($this->configuration['end'])) ? DrupalDateTime::createFromTimestamp($this->configuration['end']) : '';
+    $form['start'] = array(
+      '#type' => 'datetime',
+      '#title' => t('Publish Date'),
+      '#default_value' => $default_start,
+    );
 
-        return $status;
-    }
+    $form['end'] = array(
+      '#type' => 'datetime',
+      '#title' => t('Expiry Date'),
+      '#default_value' => $default_end,
+    );
 
-    /**
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
    * {@inheritdoc}
    */
-    public function defaultConfiguration()
-    {
-        return array('start' => '', 'end' => '') + parent::defaultConfiguration();
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+
+    if (is_object($form_state->getValue('start'))) {
+      $this->configuration['start'] = $form_state->getValue('start')->getTimestamp();
     }
+    if (is_object($form_state->getValue('end'))) {
+      $this->configuration['end'] = $form_state->getValue('end')->getTimestamp();
+    }
+
+    parent::submitConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate() {
+
+    $status = TRUE;
+    if (empty($this->configuration['start']) &&  empty($this->configuration['end']) && !$this->isNegated()) {
+      return TRUE;
+    }
+    if (!empty($this->configuration['start'])) {
+      $status = $status && time() >= $this->configuration['start'];
+    }
+
+    if (!empty($this->configuration['end'])) {
+      $status = $status && time() <= $this->configuration['end'];
+    }
+
+    return $status;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+
+    return array('start' => '', 'end' => '') + parent::defaultConfiguration();
+  }
+
 }
